e83fc2911efa4a62f2e8999aa228e3340755a6aa
Minh Duke <64729573+TPODAvia@users.noreply.github.com>
2023-07-28 23:24:15 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j1
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j1
 
 cd ../../Sophus
 
@@ -22,7 +22,19 @@ echo "Configuring and building Thirdparty/Sophus ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j1
+sudo make install
+
+echo "Configuring and building Pangolin ..."
+
+cd ../..
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+mkdir build
+cd build
+cmake ..
+make -j1
+sudo make install
 
 cd ../../../
 
@@ -37,4 +49,4 @@ echo "Configuring and building ORB_SLAM3 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+make -j1
